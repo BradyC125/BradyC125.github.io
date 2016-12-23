fcad53f4fbbd87b929ad065c557bf62264631f58
BradyC125 <bradycrimmins@gmail.com>
2016-12-23 13:30:12 -0500

    Edit JS function

--- js/myScriptDoc.js
+++ js/myScriptDoc.js
@@ -3,7 +3,7 @@ $(function(){
   var navBar = $("#navBar");
 
   navBar.on("scroll", function(e) {
-    if (this.scrollTop > 147) {
+    if (this.scrollTop > 30) {
       navBar.addClass("stickyNavBar");
     } else {
       navBar.removeClass("stickyNavBar");
