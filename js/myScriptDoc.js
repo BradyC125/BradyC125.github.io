ee536853414b4a2d17dec952bafb66b08dd08a6e
BradyC125 <bradycrimmins@gmail.com>
2016-12-23 14:20:59 -0500

    Transfer JS to own doc

--- js/myScriptDoc.js
+++ js/myScriptDoc.js
@@ -1,9 +1,13 @@
-$("p").css("color", "red");
-var navBar = $("#navbar");
-navBar.on("scroll", function() {
-    if (this.scrollTop > 30) {
-        navBar.addClass("stickyNavBar");
-    } else {
-        navBar.removeClass("stickyNavBar");
-  }
-}
+$(document).ready(function(){
+    alert("This site is under development!");
+    $("p").text("Hello World!");
+    $("p").css("color", "red");
+    var navBar = $("#navbar");
+    navBar.on("scroll", function() {
+        if (this.scrollTop > 30) {
+            navBar.addClass("stickyNavBar");
+        } else {
+            navBar.removeClass("stickyNavBar");
+        }
+    }
+});
