355b0b969772800b93159839920b66afc2e9506f
BradyC125 <bradycrimmins@gmail.com>
2016-12-23 14:22:14 -0500

    Testing applicability

--- js/myScriptDoc.js
+++ js/myScriptDoc.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     alert("This site is under development!");
-    $("p").text("Hello World!");
+    $("p").text("Test2!");
     $("p").css("color", "red");
     var navBar = $("#navbar");
     navBar.on("scroll", function() {
