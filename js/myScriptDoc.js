51e0997e78baa094fdd433ae9fb2109675dc7237
BradyC125 <bradycrimmins@gmail.com>
2016-12-23 13:26:50 -0500

    Add JS function calling stickyNavBar

--- js/myScriptDoc.js
+++ js/myScriptDoc.js
@@ -1,2 +1,12 @@
-var navBar = document.getElementById("navbar");
-navBar.style.position = 'fixed';
+$(function(){
+  
+  var navBar = $("#navBar");
+
+  navBar.on("scroll", function(e) {
+    if (this.scrollTop > 147) {
+      navBar.addClass("stickyNavBar");
+    } else {
+      navBar.removeClass("stickyNavBar");
+  }
+  
+});
