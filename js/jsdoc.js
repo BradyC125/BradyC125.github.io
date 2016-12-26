3aa04f1c97aca88bcd4d8a8fbca0cf6c36acb0e2
BradyC125 <bradycrimmins@gmail.com>
2016-12-26 14:22:46 -0500

    Test new scroll-then-stick

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
     alert("alert1");
     var navBar = $("#navbar");
-    $(document).on("scroll", function(e) {
-        if (this.scrollTop > 30) {
-            alert("alert2");
-            navBar.addClass("stickyNavBar");
-        } else {
-            navBar.removeClass("stickyNavBar");
-        }
+    navBar.toggleClass("stickyNavBar", this.scrollTop > 147);
     })
 });
