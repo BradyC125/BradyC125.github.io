3f14de2d32370f87d6989d2f742135134e7e86b7
BradyC125 <bradycrimmins@gmail.com>
2016-12-26 13:25:26 -0500

    Test new function criterion

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -2,7 +2,7 @@ alert("alert1")
 $(document).ready(function(){
     alert("alert2")
     var navBar = $("#navbar");
-    navBar.on("scroll", function(e) {
+    $(document).on("scroll", function(e) {
         alert("alert")
         if (this.scrollTop > 30) {
             navBar.addClass("stickyNavBar");
