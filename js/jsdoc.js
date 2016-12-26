a63de8dc2ea00448a1c02129d03c5ba070a43f75
BradyC125 <bradycrimmins@gmail.com>
2016-12-26 13:18:27 -0500

    test usage of js funct 2

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var navBar = $("#navbar");
     navBar.on("scroll", function(e) {
+        alert("alert")
         if (this.scrollTop > 30) {
             navBar.addClass("stickyNavBar");
         } else {
