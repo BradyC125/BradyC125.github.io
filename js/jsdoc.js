02363c6d8afafba2fd625ffc201c6ce22165e36a
Brady <bradycrimmins@gmail.com>
2017-01-09 18:41:45 -0500

    Test scroll JS

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
-    var navBar = $("#navbar");
-    navBar.toggleClass("stickyNavBar", this.scrollTop > 30);
-});
+   var elementPosition = $('#navbar').offset();
+    $(window).scroll(function () {
+    if ($(window).scrollTop() > elementPosition.top) {
+        $('#navbar').css('position', 'fixed').css('top', '0');
+    } else {
+        $('#navbar').css('position', 'static');
+    }
+  }});
+
