6937bde9c912198bb7290f560702c35a2c1fd184
Brady <bradycrimmins@gmail.com>
2017-01-09 18:59:48 -0500

    Chain selectors test

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
    var elementPosition = $('#navbar').offset();
     $(window).scroll(function () {
     if ($(window).scrollTop() > elementPosition.top) {
-       $('#navbar').css('position', 'fixed').css('top', '0').css('text-align', 'center').css('align', 'center');
+       $('#navbar').css('position', 'fixed').css('top', '0')
+       $('#navbar > ul').css('text-align', 'center');
     } else {
         $('#navbar').css('position', 'static');
     }
