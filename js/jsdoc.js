e13c7832f533012198556403541313f36a840889
Brady <bradycrimmins@gmail.com>
2017-01-09 19:24:52 -0500

    remove text-align: center;

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
    var elementPosition = $('#navbar').offset();
     $(window).scroll(function () {
     if ($(window).scrollTop() > elementPosition.top) {
-       $('#navbar').css('position', 'fixed').css('top', '0').css('text-align', 'center');
+       $('#navbar').css('position', 'fixed').css('top', '0')
     } else {
         $('#navbar').css('position', 'static');
     }
