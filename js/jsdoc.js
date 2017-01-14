7255da7a46703b406588d6d0d14f08d806c5dc5a
Brady <bradycrimmins@gmail.com>
2017-01-14 18:25:50 -0500

    Fine tune top more

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    var elementPosition = $('#navbar').offset();
     $(window).scroll(function () {
-    if ($(window).scrollTop() > elementPosition.top-7) {
+    if ($(window).scrollTop() > elementPosition.top-14) {
        $('#navbar').css('position', 'fixed').css({'top':'0','left':'0','right':'0'}).css('text-align', 'center');
     } else {
         $('#navbar').css('position', 'static');
