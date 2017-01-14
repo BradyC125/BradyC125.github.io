1ee966b7c1eb829be131b74a6be6496d91a4513c
Brady <bradycrimmins@gmail.com>
2017-01-14 17:13:29 -0500

    Align the bar center

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
    var elementPosition = $('#navbar').offset();
     $(window).scroll(function () {
     if ($(window).scrollTop() > elementPosition.top) {
-       $('#navbar').css('position', 'fixed').css('top', '0')
+       $('#navbar').css('position', 'fixed').css('top', '0').css('align', 'center');
     } else {
         $('#navbar').css('position', 'static');
     }
