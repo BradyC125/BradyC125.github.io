944f2bb90fc4ddbfd4168e22c2280e4af2a5e70e
Brady <bradycrimmins@gmail.com>
2017-01-14 18:23:27 -0500

    Fine tune distance on sticky

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    var elementPosition = $('#navbar').offset();
     $(window).scroll(function () {
-    if ($(window).scrollTop() > elementPosition.top-10) {
+    if ($(window).scrollTop() > elementPosition.top-7) {
        $('#navbar').css('position', 'fixed').css({'top':'0','left':'0','right':'0'}).css('text-align', 'center');
     } else {
         $('#navbar').css('position', 'static');
