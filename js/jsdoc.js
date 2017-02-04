f80d1592b2108f7daab13fe74bc81c4013649332
Brady <bradycrimmins@gmail.com>
2017-02-03 19:14:04 -0500

    Attempt to fix pagejump

--- js/jsdoc.js
+++ js/jsdoc.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     if ($(window).scrollTop() > elementPosition.top) {
        $('#navbar').css('position', 'fixed').css({'top':'0','left':'0','right':'0'}).css('text-align', 'center');
        $('#mainContent').css('margin-top', '57.440px'); 
+       $('#content').css('margin-top', '33px');
     } else {
        $('#navbar').css('position', 'static');
        $('#mainContent').css('margin-top', '0px'); 
